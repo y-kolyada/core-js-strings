bf07019312360d95d34f77bf75e0edf1d2c317c0
ikclouds <ikclouds91@gmail.com>
2025-09-21 21:11:21 +0300

    feat: concatenateStrings

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -56,8 +56,11 @@ function isString(value) {
  *   concatenateStrings('aa', '') => 'aa'.
  *   concatenateStrings('', 'bb') => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  if (typeof value1 === 'string' && typeof value2 === 'string') {
+    return value1.concat(value2);
+  }
+  return '';
 }
 
 /**
