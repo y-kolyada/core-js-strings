a6132ca5a4428821dade3b763890476fb1a13dcd
ikclouds <ikclouds91@gmail.com>
2025-09-21 21:13:32 +0300

    feat: removeLeadingAndTrailingWhitespaces

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -92,8 +92,11 @@ function getFirstChar(value) {
  *   removeLeadingAndTrailingWhitespaces('cat ') => 'cat'
  *   removeLeadingAndTrailingWhitespaces('\t\t\tHello, World! ') => 'Hello, World!'
  */
-function removeLeadingAndTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeLeadingAndTrailingWhitespaces(value) {
+  if (typeof value === 'string') {
+    return value.trim();
+  }
+  return '';
 }
 
 /**
