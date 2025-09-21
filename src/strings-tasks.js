a9a674b7f72527d8e8cf41c15baa06fa5422bcd3
ikclouds <ikclouds91@gmail.com>
2025-09-21 21:18:39 +0300

    feat: repeatString

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -148,8 +148,11 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (typeof str === 'string' && typeof times === 'number' && times > 0) {
+    return str.repeat(times);
+  }
+  return '';
 }
 
 /**
